2b84d34df376e73fbddc4e5fa092711583266218
Pragyam Soni <beyondyourunderstanding479@gmail.com>
2022-01-11 22:25:08 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+nlcmzlzcyj_w&@1bu5-fr(+nwn%dr(tstvfiaohys!-bxtzvm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'editnote.herokuapp.com'   
